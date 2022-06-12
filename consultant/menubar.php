9fd949f07b6d378f44bff131ba567f8753ae9b5a
MohammedKhaled98 <102185723+MohammedKhaled98@users.noreply.github.com>
2022-06-12 20:25:05 +0300

    Add files via upload

---
+++ consultant/menubar.php
@@ -0,0 +1,28 @@
+﻿<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          
+        </div>
+        <div class="pull-left info">
+          <p><?php echo $user['firstname'].' '.$user['lastname']; ?></p>
+         
+        </div>
+      </div>
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">تحكم  </li>
+        <li class=""><a href="home.php"><i class="fa fa-dashboard"></i> <span>لوحة التحكم</span></a></li>
+        <li class="header"> الصفحات </li>
+        
+        <li><a href="myapp.php"><i class="material-icons">group</i> <span>مواعيدي</span></a></li>
+       <li><a href="profile.php"><i class="material-icons">group</i> <span>ادارة الملف الشخصي</span></a></li>
+
+      
+      
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
